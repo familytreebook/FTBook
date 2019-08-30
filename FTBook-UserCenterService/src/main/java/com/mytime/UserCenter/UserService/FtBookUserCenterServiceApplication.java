c6d52e2a588ed8ceede90165487d0b93dd0f7941
senwa <503704814@qq.com>
2019-08-30 15:59:33 +0800

    用户中心微服务项目

---
+++ FTBook-UserCenterService/src/main/java/com/mytime/UserCenter/UserService/FtBookUserCenterServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mytime.UserCenter.UserService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtBookUserCenterServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FtBookUserCenterServiceApplication.class, args);
+	}
+
+}
