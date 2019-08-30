a64a00918dab82a697e49a0a65654d89d0b224c3
senwa <503704814@qq.com>
2019-08-30 16:07:16 +0800

    多媒体文件数据管理服务项目

---
+++ FTBook-MediaCenterService/src/main/java/com/mytime/MediaCenter/MediaService/FtBookMediaCenterServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mytime.MediaCenter.MediaService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtBookMediaCenterServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FtBookMediaCenterServiceApplication.class, args);
+	}
+
+}
