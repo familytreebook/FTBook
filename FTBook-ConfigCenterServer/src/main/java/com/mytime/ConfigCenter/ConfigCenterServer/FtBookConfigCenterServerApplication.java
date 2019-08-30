ce13c7aa377b2a367b7ec95074486f51b162a3e8
senwa <503704814@qq.com>
2019-08-30 16:28:38 +0800

    配置中心服务

---
+++ FTBook-ConfigCenterServer/src/main/java/com/mytime/ConfigCenter/ConfigCenterServer/FtBookConfigCenterServerApplication.java
@@ -0,0 +1,13 @@
+package com.mytime.ConfigCenter.ConfigCenterServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtBookConfigCenterServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FtBookConfigCenterServerApplication.class, args);
+	}
+
+}
---
+++ FTBook-ConfigCenterServer/src/test/java/com/mytime/ConfigCenter/ConfigCenterServer/FtBookConfigCenterServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mytime.ConfigCenter.ConfigCenterServer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FtBookConfigCenterServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
